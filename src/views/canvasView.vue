49f39b849ded5b50010864ae0ee792e532812ad5
ssje <2114714800@qq.com>
2024-09-25 10:52:46 +0800

    主动释放gl缓存

--- src/views/canvasView.vue
+++ src/views/canvasView.vue
@@ -45,9 +45,10 @@ class canvasDraw {
     }
   }
   onHandleDraw() {
-    const gl = this.canvas.getContext('webgl')
+    const gl = this.canvas.getContext('webgl2') || this.canvas.getContext('webgl')
     gl && gl.drawArrays(gl.POINTS, 0, 1)
     const _this = this
+
     //监听鼠标按下事件
     this.canvas.addEventListener('mousedown', event => {
       event.preventDefault()
@@ -57,6 +58,7 @@ class canvasDraw {
       this.ctx.moveTo(event.offsetX, event.offsetY)
       this.canvas.addEventListener('mousemove', this.handleDraw.bind(this))
     })
+
     //监听鼠标滑动事件
     // canvas.addEventListener('mousemove', handleDraw)
 
@@ -68,10 +70,47 @@ class canvasDraw {
       this.canvas.removeEventListener('mousemove', this.handleDraw.bind(this))
     })
   }
+
+  initShader(vertexShaderSource, fragmentShaderSource) {
+    console.log('initShader', this, this.canvas)
+    const gl = this.canvas.getContext('webgl')
+    console.log('gl', gl)
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER) //创建顶点着色器对象
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER) //创建片元着色器对象
+    gl.shaderSource(vertexShader, vertexShaderSource) //引入顶点、片元着色器源代码
+    gl.shaderSource(fragmentShader, fragmentShaderSource)
+    gl.compileShader(vertexShader) //编译顶点、片元着色器
+    gl.compileShader(fragmentShader)
+    const program = gl.createProgram() //创建程序对象program
+    gl.attachShader(program, vertexShader) //附着顶点着色器和片元着色器到program
+    gl.attachShader(program, fragmentShader)
+    gl.linkProgram(program) //链接program
+    gl.useProgram(program) //使用program
+    return program //返回程序program对象
+  }
+
   //清除绘图
   clearDraw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
   }
+}
+
+function test() {
+  //顶点着色器代码
+  const vertexShaderSource = `
+  attribute vec4 a_Position;
+  void main() {
+    gl_Position = vec(0.0,0.0,0.0,1.0);
+    gl_PointSize = 20.0;
+  }
+`
+  //片元着色器代码
+  const fragmentShaderSource = `
+  void main() {
+    gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+  }
+`
+  var program = ctx.initShader(vertexShaderSource, fragmentShaderSource)
 }
 
 //保存canvas画布
@@ -88,6 +127,16 @@ onMounted(() => {
   ctx = new canvasDraw(canvas)
   ctx.init()
   ctx.onHandleDraw()
+  setTimeout(() => {
+    test()
+  }, 1000)
+  // test()
+})
+
+onBeforeUnmount(() => {
+  //主动释放GL缓存
+  let canvas = ctx?.canvas.getContext('webgl2') || ctx?.canvas.getContext('webgl')
+  canvas?.getExtension('WEBGL_lose_context')?.loseContext()
 })
 </script>
 
